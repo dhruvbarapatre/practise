ab86ed61867484aae83fa1261bfeaefca6ca20de
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2024-12-10 09:32:39 +0530

    u[date

--- auth/index.js
+++ auth/index.js
@@ -1,1 +1,14 @@
-const express=require("express")
+const express = require("express")
+const connection = require("./config/db")
+
+const app = express()
+const dotenv = require("dotenv").config()
+
+app.listen(process.env.port, async () => {
+    try {
+        await connection
+        console.log("Connected TO DB......")
+    } catch (error) {
+        console.log(error)
+    }
+})
