5c8850d122c09511e779751a9c5be2619de5a159
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2024-12-13 08:52:34 +0530

    up

--- auth/index.js
+++ auth/index.js
@@ -2,12 +2,14 @@ const express = require("express")
 const connection = require("./config/db")
 const { userRouter } = require("./routes/user.routes")
 var cookieParser = require('cookie-parser')
+const { movieRoutes } = require("./routes/movies.routes")
 
 const app = express()
 const dotenv = require("dotenv").config()
 app.use(cookieParser())
 app.use(express.json())
 app.use("/user",userRouter)
+app.use("/movies",movieRoutes)
 
 app.listen(process.env.port, async () => {
     try {
--- auth/routes/movies.routes.js
+++ auth/routes/movies.routes.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const movieRoutes = express()
 
-movieRoutes.get("/get", () => {
+movieRoutes.get("/get", (req,res) => {
     res.send(req.cookies)
 })
+
+module.exports={movieRoutes}
