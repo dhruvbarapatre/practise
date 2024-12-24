36a4a92591b1e094f79895d63b9935069c1ce913
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2024-12-24 08:56:57 +0530

    update

--- multer/client/src/App.jsx
+++ multer/client/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [file, setfile] = useState([])
+  const [file, setfile] = useState(null)
   const onchange=()=>{
     console.log(file)
+    axios.post("",{file},{
+      headers:{
+        "Content-Type":"multipart/from-data"
+      }
+    })
   }
   return (
     <>
-      <input type="file" onChange={(e)=>setfile(e.target.files)}/>
+      <input type="file" onChange={(e)=>setfile(e.target.files[0])}/>
       <button onClick={onchange}>Upload</button>
     </>
   )
--- multer/server/index.js
+++ multer/server/index.js
@@ -1,1 +1,15 @@
-const express=require("express")
+const express = require("express")
+
+const app = express()
+
+app.post("/upload", (req, res) => {
+
+})
+
+app.get("/images", (req, res) => {
+
+})
+
+app.listen(8080, () => {
+    console.log("Server is running")
+})
