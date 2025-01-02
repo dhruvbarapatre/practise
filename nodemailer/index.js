07a823d40850d003fa4dfe13339dc48fa1f12360
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2025-01-02 19:44:17 +0530

    update

---
+++ nodemailer/index.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer")
+
+const transpoter = nodemailer.createTransport({
+    secure: true,
+    host: "smtp.gmail.com",
+    port: 465,
+    auth: {
+        user: "barapatredhruv28012006@gmail.com",
+        pass: "kqqxouklyvqynhdk"
+    }
+})
+
+async function sendmail(to, sub, html) {
+    try {
+        const data = await transpoter.sendMail({
+            to,
+            sub,
+            html,
+            from:' barapatredhruv28012006@gmail.com'
+        })
+        console.log("Mail Sent Succesfully.....",data)
+    } catch (error) {
+        console.log(error)
+    }
+
+    
+}
+
+sendmail("barapatremonish051127@gmail.com", "hi bro", "<h1>Gandu<h1/>")
